66713e8ce42500245ed4aa3858e0e2fda4eb9d61
Muhammad Muneeb <rattlinglord999@gmail.com>
2023-06-05 22:13:50 +0500

    Updated

--- app/src/main/java/com/example/quranapplication/MainActivity2.java
+++ app/src/main/java/com/example/quranapplication/MainActivity2.java
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.sql.Array;
@@ -14,14 +18,21 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MainActivity2 extends AppCompatActivity {
+
+
+    //Declaring the object of the QDH Class so its functions can be utilised
     QDH s=new QDH();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main2);
-        System.out.println("inside function of main activity 2");
+
+        EditText editText=findViewById(R.id.edt);
+
+
+        Button btn=findViewById(R.id.btn);
+
         QuranArabicText qat=new QuranArabicText();
         Intent intent = getIntent();
         int indexSurah= intent.getIntExtra("index",-1);
@@ -39,14 +50,47 @@ public class MainActivity2 extends AppCompatActivity {
             System.out.println("How is you ");
             System.out.println(element);
         }
+ArrayList<String> adpt=new ArrayList(Surah);
 
-        if(Surah!=null)
-        {
-            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,Surah);
+            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,adpt);
             list.setAdapter(arrayAdapter);
             System.out.println("You clicked on :");
 
-        }
+
+
+        ArrayList<String> atl=new ArrayList<String>();
+       btn.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+
+               int val=Integer.parseInt(editText.getText().toString());
+
+               System.out.println("The verse number is given as follows "+val);
+               if(val>=SurahStartIndex && val<= SurahStartIndex+SurahLength )
+               {
+                   System.out.println("This value should not be out of bound"+val);
+                   String verse=Surah.get(val);
+                   System.out.println(verse);
+                   System.out.println("I am inside the button right now ");
+                   atl.add(verse);
+                   arrayAdapter.clear();
+                   arrayAdapter.addAll(atl);
+
+                   // Notify the adapter that the data has changed
+                   arrayAdapter.notifyDataSetChanged();
+                   atl.clear();
+                  // arrayAdapter.clear();
+                 //  arrayAdapter.
+
+
+               }
+           }
+       });
+
+
+
+
+
 
 
 
